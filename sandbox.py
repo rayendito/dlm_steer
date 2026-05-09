11f06f52f97f5af4393d953bb0afde9c2e8b1eac
rayendito <ryanditodua@gmail.com>
2026-05-09 16:13:12 +0000

    chat i think it workz

--- sandbox.py
+++ sandbox.py
@@ -8,9 +8,7 @@ from llada.generate import resteer_v2
 def l2_normalize(v, eps=1e-12):
     return v / (v.norm(p=2) + eps)
 
-
 EASY_EXAMPLE = "Shrek is a fun, clever twist on classic fairy tales that manages to be both hilarious and heartfelt at the same time. Instead of a typical hero, you get a grumpy but lovable ogre whose journey is full of sharp jokes, memorable moments, and a surprisingly meaningful message about acceptance and being yourself. The characters, especially Donkey and Fiona, bring tons of personality and charm, making the story feel lively and engaging from start to finish. It’s the kind of movie that works for all ages and still feels fresh even years later."
-
 HARD_EXAMPLE = "I had started to lose my faith in films of recent being inundated with the typical Genre Hollywood film. Story lines fail, and camera work is merely copied from the last film of similiar taste. But, then I saw Zentropa (Europa) and my faith was renewed. Not only is the metaphorical storyline enthralling but the use of color and black and white is visually stimulating. The narrator (Max Von Sydow) takes you through a spellbounding journey every step of the way and engrosses you into Europa 1945. We have all seen death put on screen in a hundred thousand ways but the beauty of this film is how it takes you through every slow-moving moment that leads you to death."
 STEER_VECTORS = "steer_vectors/diffusion-debug_3.pt"
 STEER_DIRECTION = "negative"
@@ -28,7 +26,7 @@ steer_vectors_all = tuple(
 if STEER_DIRECTION == "positive":
     steer_vectors_all = tuple(-v for v in steer_vectors_all)
 
-steer_alpha = 100
+steer_alpha = 450
 steer_layer = [16, 25, 31]
 steer_vectors = {si: steer_alpha * steer_vectors_all[si] for si in steer_layer}
 
@@ -45,8 +43,8 @@ tokenizer = AutoTokenizer.from_pretrained(
 )
 tokenizer.padding_side = "left"
 
-RESTEER_STEPS = 3
-REFILL_STEPS = 10
+RESTEER_STEPS = 5
+REFILL_STEPS = 20
 
 tokenized_inputs = tokenizer(
     [EASY_EXAMPLE, HARD_EXAMPLE],
